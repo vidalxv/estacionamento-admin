2abf4ff2a5a57498edbdb62d93355d4ff4eab4ce
Roque Maia <roquemaiarocha08@gmail.com>
2024-05-16 19:12:15 -0300

    add telas

--- src/js/script.js
+++ src/js/script.js
@@ -34,16 +34,3 @@ window.onclick = function(event) {
         popup.style.display = "none";
     }
 }
-
-function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  const main = document.getElementById("main");
-
-  if (sidebar.style.width === "250px") {
-      sidebar.style.width = "0";
-      main.style.marginLeft = "0";
-  } else {
-      sidebar.style.width = "250px";
-      main.style.marginLeft = "250px";
-  }
-}
