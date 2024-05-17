18b322ca1f911cc5577ac651b9efe3221fd936eb
Daniel Vidal <vidalffyt@gmail.com>
2024-05-17 10:37:54 -0300

    adicao de elementos nas paginas

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,4 @@
-// Obtém o modal
-var popup = document.getElementById("myPopup");
 
-// Obtém o botão que abre o modal
-var btn = document.getElementById("openPopupBtn");
-
-// Obtém o elemento <span> que fecha o modal
-var span = document.getElementById("closePopupBtn");
-
-// Quando o usuário clica no botão, abre o modal
-btn.onclick = function() {
-    popup.style.display = "block";
-}
-
-// Quando o usuário clica no <span> (x), fecha o modal
-span.onclick = function() {
-    popup.style.display = "none";
-}
-
-// Quando o usuário clica em qualquer lugar fora do modal, fecha o modal
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const vagaButtons = document.querySelectorAll('.vaga');
